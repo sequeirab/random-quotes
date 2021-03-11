12d793c40209ae7623a277eac3f132f879fb2b0e
Bruno Sequeira <sequeirab86@gmail.com>
2021-03-11 23:36:20 +0000

    pass tests but has some bugs

--- src/App.js
+++ src/App.js
@@ -17,19 +17,19 @@ class App extends React.Component {
         },
         {
           quote: '1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-          author: 'Lorem ipsum',
+          author: '1 Lorem ipsum',
         },
         {
           quote: '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-          author: 'Lorem ipsum',
+          author: '2 Lorem ipsum',
         },
         {
           quote: '3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-          author: 'Lorem ipsum',
+          author: '3 Lorem ipsum',
         },
         {
           quote: '4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-          author: 'Lorem ipsum',
+          author: '4 Lorem ipsum',
         },
       ],
 
@@ -37,32 +37,23 @@ class App extends React.Component {
     }
 
     this.changeQuote = this.changeQuote.bind(this);
-    this.firstRandom = this.firstRandom.bind(this);
   }
 
   
-
+componentDidMount() {
+  let length = this.state.quotes.length
+  if(this.state.i === length - 1) {
+    this.setState({i: 0})
+  }
+}
 
   changeQuote() {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    let length = this.state.quotes.length  
-    this.setState({i: getRandomInt(length)})
+    
+      
+    this.setState({i: this.state.i + 1})
   
     
-  }
-
-  firstRandom() {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-    let length = this.state.quotes.length
-    return this.setState({i: getRandomInt(length)})
-  }
-  
-  
+  }  
 
 
 
@@ -71,7 +62,7 @@ class App extends React.Component {
     return (
     <div className='body'>
       
-    <div onLoad={this.firstRandom} id='quote-box'>
+    <div id='quote-box'>
       <div id='text'>{this.state.quotes[this.state.i].quote}</div>
       <div id='author'>{this.state.quotes[this.state.i].author}</div>
       <div className='flex'>
